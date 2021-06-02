e0ecc9f96c347ac0e88c39eaf1d4fd0f14240109
bryanronnie <bryanronniej18023@it.ssn.edu.in>
2021-06-02 14:28:16 +0530

    build

---
+++ build/precache-manifest.1e1e702c2dfb93d57716386304e56dd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3045994884e0c7b1d0e6b6d561b09e75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2e976aee89bed1d043f",
+    "url": "/static/css/2.c8e004df.chunk.css"
+  },
+  {
+    "revision": "07970cfa80ad6bef4323",
+    "url": "/static/css/main.b43806f3.chunk.css"
+  },
+  {
+    "revision": "d2e976aee89bed1d043f",
+    "url": "/static/js/2.ed44898d.chunk.js"
+  },
+  {
+    "revision": "ed344764e96e72a32d0bd51aea29b8b2",
+    "url": "/static/js/2.ed44898d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07970cfa80ad6bef4323",
+    "url": "/static/js/main.db2c3db2.chunk.js"
+  },
+  {
+    "revision": "5ff6df50c1a535018958",
+    "url": "/static/js/runtime-main.df338948.js"
+  }
+]);
